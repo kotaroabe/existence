a71fbc208c1bcc795d1499a7754efd4f76b7c048
kotaroabe <t11029ka@gmail.com>
2014-03-15 23:56:02 +0900

    unsettled project

--- unsettled_existence/unsettled_existence.ino
+++ unsettled_existence/unsettled_existence.ino
@@ -1,1 +1,18 @@
 const int GM = 6; //connect Pin 6 to a Geigercounter's OUT Pin
+const int SSR = 13; //13 Pin to the SSR
+int null;
+
+void setup()
+{
+  pinMode(GM, INPUT);
+  pinMode(LED,OUTPUT);
+}
+
+void loop()
+{
+  /* everytime a Geigercounter detects radiation */
+  while(digitalRead(GM) == null) {} 
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+} 
